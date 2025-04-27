32e25b29dd9af9d06b062e2318aba203f0af15d2
0xTunay <tigrus232323k@gmail.com>
2025-04-27 17:58:59 +0300

    delete lib

--- main.c
+++ main.c
@@ -2,12 +2,7 @@
 // Created by tunay on 4/23/25.
 //
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+
 
 #include "include/server.h"
 
