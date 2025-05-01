8b28c6a6cd655b713850d6b6830a9fc3c04ac358
0xTunay <tigrus232323k@gmail.com>
2025-05-01 12:24:35 +0300

    add memory allocated

--- src/request.c
+++ src/request.c
@@ -18,15 +18,43 @@
 int request_init(int clientfd){
     struct Request requests;
 
-    char request[4096];
+    int total_bytes = 0;
+    int buffer_size = 1024;
 
-    int received = recv(clientfd, request, sizeof(request), 0);
-    if(received == -1) {
-        perror("recv");
+    char *request = NULL;
+    request = (char*)malloc(buffer_size);
+    if (!request) {
         return -1;
+
     }
+    memset(request, 0, buffer_size);
+
+    while (strtok(request, "\r\n\r\n") == NULL) {
+        int received = recv(clientfd, request+ total_bytes, buffer_size - total_bytes, 0 );
+        if (received == -1) {
+            free(request);
+            return -1;
+        }
+        if (received == 0) {
+            close(clientfd);
+            break;
+        }
+
+        total_bytes += received;
+        request[total_bytes] = '\0';
+
+        if (buffer_size - total_bytes < 128) {
+            buffer_size *= 2;
+            char *new_request = (char*) realloc(request, sizeof(char) * buffer_size);
+            if (!new_request) {
+                free(request);
+                return -1;
+            }
+            request = new_request;
+        }
+    }
+
     printf("Request received: %s\n", request);
-    request[received] = '\0';
 
     char *method = strtok(request, " ");
     char *path = strtok(NULL, " ");
@@ -41,6 +69,6 @@ int request_init(int clientfd){
     requests.path = path;
     requests.version = version;
 
-
+    free(request);
     return 0;
 }
