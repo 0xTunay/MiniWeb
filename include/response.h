592f3d6cd81607242e7b4bd7f6fe4945199be783
0xTunay <tigrus232323k@gmail.com>
2025-05-04 16:55:13 +0300

    create new fuck(xd) and struct for server

--- include/response.h
+++ include/response.h
@@ -4,5 +4,18 @@
 
 #ifndef RESPONSE_H
 #define RESPONSE_H
+#include <stddef.h>
+
+typedef struct response {
+    int status;
+    char content_type[64];
+    char *content;
+    size_t content_length_size;
+} server_response;
+
+
+char *path = "../static/index.html";
+char *path_error = "404";
+int init_response(int clientfd, server_response *response);
 
 #endif //RESPONSE_H
