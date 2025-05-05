e547af1ee1845140a0522f7abc4e408c1e40e23e
0xTunay <tigrus232323k@gmail.com>
2025-05-05 19:05:14 +0300

    change name and struct

--- include/response.h
+++ include/response.h
@@ -6,16 +6,14 @@
 #define RESPONSE_H
 #include <stddef.h>
 
-typedef struct response {
+typedef struct {
     int status;
     char content_type[64];
     char *content;
-    size_t content_length_size;
+    size_t content_length;
 } server_response;
 
 
-char *path = "../static/index.html";
-char *path_error = "404";
 int init_response(int clientfd, server_response *response);
 
 #endif //RESPONSE_H
