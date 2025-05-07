ea0bb6aa598351ebb8ba4fe8cf27bf03710b7ce2
0xTunay <tigrus232323k@gmail.com>
2025-05-07 17:51:54 +0300

    connection with mime

--- src/response.c
+++ src/response.c
@@ -3,7 +3,7 @@
 //
 
 #include "../include/response.h"
-#include "../include/request.h"
+#include "../include/mime.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,7 +32,7 @@ int  init_response(int clientfd, server_response *response) {
             fclose(fp);
             return -1;
         } // super technology
-            if (size_file >= LONG_MAX) {
+        if (size_file >= LONG_MAX) {
             fprintf(stderr, "File size too large\n");
             fclose(fp);
             return -1;
@@ -53,9 +53,18 @@ int  init_response(int clientfd, server_response *response) {
         fclose(fp);
         response->content = buffer;
 
+        const char *extention = strrchr(path, '.');
+        if (extention) {
+            extention++;
+        }else {
+            extention = "txt";
+        }
+        const char *mime_type = get_type(extention);
+
         char accept_header[256];
         snprintf(accept_header, sizeof(accept_header),
-                 "HTTP/1.1 200 OK\r\nContent-Length: %ld\r\nContent-Type: text/html\r\n\r\n");
+                "HTTP/1.1 200 OK\r\nContent-Length: %ld\r\nContent-Type: text/html\r\n\r\n",
+                response->content_length, mime_type);
         int so_fine = send(clientfd, accept_header, strlen(accept_header), 0);
         if (so_fine == -1) {
             perror("send");
@@ -69,19 +78,20 @@ int  init_response(int clientfd, server_response *response) {
 
         response->status = 200;
         response->content_length = size_file;
+        strcpy(response->content_type,mime_type);
         strcpy(response->content_type,"text/html");
 
     } else {
         const char *not_found = "HTTP/1.1 404 Not Found\r\n"
-                                "Content-Length: 13\r\n"
-                                "Content-Type: text/plain\r\n"
-                                "\r\n"
-                                "404 Not Found";
+            "Content-Length: 13\r\n"
+            "Content-Type: text/plain\r\n"
+            "\r\n"
+            "404 Not Found";
 
-            response->status = 404;
-            strcpy(response->content_type, "text/plain");
-            response->content = "404 Not Found";
-            response->content_length = 13;
+        response->status = 404;
+        strcpy(response->content_type, "text/plain");
+        response->content = "404 Not Found";
+        response->content_length = 13;
 
         int so_sad = send(clientfd,not_found,strlen(not_found),0);
         if (so_sad == -1) {
