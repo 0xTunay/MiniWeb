fb944f9c73915b90ff3a64cb207f52b71ed65724
0xTunay <tigrus232323k@gmail.com>
2025-05-18 16:13:26 +0300

    try doxygen technology

--- main.c
+++ main.c
@@ -6,6 +6,11 @@
 
 #include "include/server.h"
 
+/**
+ * @author Tunay
+ * @data 23 04 2025
+ * @details main page for start program
+ */
 int main(void) {
     puts("Mini web server with C lang");
     server_init();
